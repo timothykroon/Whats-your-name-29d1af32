9bbebd16ba318099d792c14567d93a51d2dd80b1
Timothy Kroon <timothykroon1@gmail.com>
2020-03-03 12:17:32 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "wat is je voornaam" . PHP_EOL;
+$voornaam = readline();
+echo "wat is je achternaam". PHP_EOL;
+$achternaam = readline();
+echo "jouw naam is: " . $voornaam . " " . $achternaam;
+
+?>
